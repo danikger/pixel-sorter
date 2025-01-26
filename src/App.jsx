705f2fd5738cb771d01deb2364f7fd7bf6c24218
Daniil Gerachshenko <danikger@gmail.com>
2025-01-25 18:29:51 -0600

    Adds functionality to main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,49 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import processImage from './Components/processImage';
+import FileDropzone from './Components/fileDropzone';
+import ImageComparison from './Components/imageComparison';
+import './App.css';
 
 function App() {
+  const [imageURL, setImageURL] = useState("");
+  const [sortedImage, setSortedImage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  /**
+   * Handles the file upload event from the dropzone component.
+   * 
+   * @param {File} image Image file that was uploaded through the dropzone. 
+   */
+  function handleFileUpload(image) {
+    const url = URL.createObjectURL(image);
+    setImageURL(url);
+  }
+
+  useEffect(() => {
+    async function processImageAsync() {
+      if (imageURL) {
+        try {
+          setLoading(true);
+          const sorted = await processImage(imageURL);
+          setSortedImage(sorted);
+          setLoading(false);
+        } catch (error) {
+          // console.error("Error processing image: ", error);
+        }
+      }
+    };
+
+    processImageAsync();
+  }, [imageURL]);
+
   return (
     <>
-      
+      <main className="bg-zinc-100 min-h-screen relative size-full px-4">
+        <div className="max-w-screen-lg mx-auto pt-10 sm:pt-16 px-2 pb-16">
+          <FileDropzone className="mb-28 sm:w-1/2" onFileUpload={handleFileUpload} />
+          <ImageComparison imageURL={imageURL} sortedURL={sortedImage} loading={loading} />
+        </div>
+      </main>
     </>
   )
 }
